fcdf0d1715c6f278fabe17d37e7c4bd890f4fabe
a-motthet <atsadawut.mott@gmail.com>
2024-11-18 18:28:51 +0000

    bruhhhhhhhh

--- test_zoo.py
+++ test_zoo.py
@@ -6,7 +6,19 @@ class TestZoo(unittest.TestCase):
         self.zoo = Zoo()
 
     def test_child_ticket_price(self):
-        self.assertEqual(self.zoo.get_ticket_price(5), 50)
+        self.assertEqual(self.zoo.get_ticket_price(-1), "age error")
+
+    def test_child_ticket_price1(self):
+        self.assertEqual(self.zoo.get_ticket_price(11), 50)
+
+    def test_child_ticket_price2(self):
+        self.assertEqual(self.zoo.get_ticket_price(13),  100)
+
+    def test_child_ticket_price3(self):
+        self.assertEqual(self.zoo.get_ticket_price(60),  150)
+
+    def test_child_ticket_price4(self):
+        self.assertEqual(self.zoo.get_ticket_price(61),  100)
        
     # Add your additional test cases here.
 
